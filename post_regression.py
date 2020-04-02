4a61ba162ff57c72e143ffa136ee35fbdede87c0
felipehuerta17 <f.huerta-perez17@imperial.ac.uk>
2020-04-02 22:41:51 +0100

    feat(model): multi parameter fitting
    This took a long while as scipy.optimize(x,*p) takes a list of separate args
    and not a vector list. I hardcoded for the case of SIR (two parameters).
    
    Tomorrow I will extend it also to work for SIR-X and it is fine for the release,
    but the code can be much improved without changing the functionality and extending
    to an arbitrary length of parameters. I don't know how to do this at the moment
    and creating a new minimizing function is not practical.

--- post_regression.py
+++ post_regression.py
@@ -2,7 +2,10 @@ import model
 import numpy as np
 from sklearn.utils import resample
 
-def ci_bootstrap(model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_ci = True):
+
+def ci_bootstrap(
+    model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_ci=True
+):
     """
     
     Calculates the confidence interval of the parameters
@@ -32,7 +35,7 @@ def ci_bootstrap(model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_
     of models. 
     """
 
-    p0 = model.p 
+    p0 = model.p
     w0 = model.w0
 
     p_bt = []
@@ -40,7 +43,7 @@ def ci_bootstrap(model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_
         r0_bt = []
 
     # Perform bootstraping
-    for i in range(0,n_iter):
+    for i in range(0, n_iter):
         t_r, I_r = resample(t_obs, n_I_obs)
         I_r = np.array(I_r)
         # Sort and redefine arrays
@@ -49,19 +52,19 @@ def ci_bootstrap(model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_
         I_rs = I_r[idx]
         # Fit the model to the sampled data
         # Update model initial conditions
-        I0_rs = I_rs[0]/population
-        S0_rs = (population-I0_rs)/population
-        R0_rs = 0 # We will still assume that we don't have observations of recovered
+        I0_rs = I_rs[0] / population
+        S0_rs = (population - I0_rs) / population
+        R0_rs = 0  # We will still assume that we don't have observations of recovered
 
         model.w0 = [S0_rs, I0_rs, R0_rs]
         model.fit(t_rs, I_rs, population)
         p_bt.append(model.p)
         if r0_ci:
             r0_bt.append(model.r0)
-    
+
     # Calculate percentiles value
-    p_low = ((1-alpha)/2)*100
-    p_up = (alpha + (1-alpha)/2)*100
+    p_low = ((1 - alpha) / 2) * 100
+    p_up = (alpha + (1 - alpha) / 2) * 100
     # From the resulting distribution, extract the
     # percentile value of the parameters
 
@@ -69,23 +72,25 @@ def ci_bootstrap(model, t_obs, n_I_obs, population, alpha=0.95, n_iter=1000, r0_
 
     # Construct confidence intervals
     ci = []
-    for i in range(0,len(model.p)):
-        ci_low = np.percentile(p_bt[:,i], p_low)
-        ci_up = np.percentile(p_bt[:,i], p_up)
-        ci.append([ci_low,ci_up])
+    for i in range(0, len(model.p)):
+        ci_low = np.percentile(p_bt[:, i], p_low)
+        ci_up = np.percentile(p_bt[:, i], p_up)
+        ci.append([ci_low, ci_up])
 
-    if r0_ci==True:
-        ci.append([np.percentile(r0_bt, p_low),
-        np.percentile(r0_bt, p_up)])
-    
+    if r0_ci == True:
+        ci.append([np.percentile(r0_bt, p_low), np.percentile(r0_bt, p_up)])
+
     ci = np.array(ci)
     # Reconstruct model original parameters
-    model.p = p0 
+    model.p = p0
     model.w0 = w0
 
     return ci, p_bt
 
-def ci_block_cv(model, t_obs, n_I_obs, population, lag = 1, min_sample=3, alpha=0.95, r0_ci = True):
+
+def ci_block_cv(
+    model, t_obs, n_I_obs, population, lag=1, min_sample=3, alpha=0.95, r0_ci=True
+):
     """ Calculates the confidence interval of the model parameters
     using a block cross validation appropriate for time series
     and differential systems when the value of the states in the
@@ -113,28 +118,28 @@ def ci_block_cv(model, t_obs, n_I_obs, population, lag = 1, min_sample=3, alpha=
     try to infer the probability density function of the parameters.
     """
 
-    p0 = model.p 
+    p0 = model.p
     w0 = model.w0
 
     # Consider at least the three first datapoints
     p_list = []
-    MSE_list = [] # List of mean squared errors of the prediction for the time t+1
-    for i in range(min_sample-1, len(n_I_obs)-lag):
+    MSE_list = []  # List of mean squared errors of the prediction for the time t+1
+    for i in range(min_sample - 1, len(n_I_obs) - lag):
         # Fit model to a subset of the time-series data
         model.fit(t_obs[0:i], n_I_obs[0:i], population)
         # Store the rolling parameters
         p_list.append(model.p)
         # Predict for the i+1 period
-        sol = model.solve(t_obs[i+lag], numpoints = t_obs[i+lag]+1)
+        sol = model.solve(t_obs[i + lag], numpoints=t_obs[i + lag] + 1)
         # Calculate mean squared errors
-        MSE = np.sqrt( (population*sol[i-1+lag,2] - n_I_obs[i-1+lag])**2 )
+        MSE = np.sqrt((population * sol[i - 1 + lag, 2] - n_I_obs[i - 1 + lag]) ** 2)
         MSE_list.append(MSE)
 
     p_list = np.array(p_list)
     MSE_list = np.array(MSE_list)
     MSE_avg = np.mean(MSE_list)
 
-    model.p=p0
-    model.w=w0
+    model.p = p0
+    model.w = w0
 
     return MSE_avg, MSE_list, p_list
