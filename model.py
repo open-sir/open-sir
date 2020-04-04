aa255e59ee20d2b69373f978eef7d6107cda3e66
Felipe Huerta <f.huerta-perez17@imperial.ac.uk>
2020-04-04 11:06:40 +0100

    feat(model): implement confidence intervals (#17)
    * chore(pipenv): add sklearn to pipenv
    
    * style(model): fix style issues
    
    * feat(model): add post regression routines
    
    * feat(model): add multivariable support for fit function
    
    * feat(model): add post regression code in Jupyter

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 """ Model implementation """
-import numpy as np # Numerical computing
-from scipy.integrate import odeint # ODE system numerical integrator
+import numpy as np  # Numerical computing
+from scipy.integrate import odeint  # ODE system numerical integrator
 from scipy.optimize import curve_fit
 
 ABSERR = 1.0e-8
 RELERR = 1.0e-6
 DAYS = 7
 NUMPOINTS = DAYS
+
 
 def call_solver(func, p, w0, t):
     """
@@ -39,11 +40,11 @@ def sir(w, t, p):
     f: right hand side of the system of differential equation
     """
     # unpack state variable
-    s, i, r = w   # pylint: disable=W0612
+    s, i, r = w  # pylint: disable=W0612
     # unpack parameter
     alpha, beta = p
-    ds_dt = -alpha*s*i
-    di_dt = alpha*s*i - beta*i
+    ds_dt = -alpha * s * i
+    di_dt = alpha * s * i - beta * i
     dr_dt = beta * i
 
     return [ds_dt, di_dt, dr_dt]
@@ -68,20 +69,20 @@ def sirx(w, t, p):
     right hand side of the system of differential equation
     """
     # unpack state variable
-    s, i, r, x = w   # pylint: disable=W0612
+    s, i, r, x = w  # pylint: disable=W0612
     # unpack parameter
     alpha, beta, kappa_0, kappa = p
-    ds_dt = -alpha*s*i - kappa_0*s
-    di_dt = alpha*s*i - beta*i - kappa_0*i - kappa*i
-    dr_dt = kappa_0*s + beta * i
+    ds_dt = -alpha * s * i - kappa_0 * s
+    di_dt = alpha * s * i - beta * i - kappa_0 * i - kappa * i
+    dr_dt = kappa_0 * s + beta * i
     dx_dt = (kappa_0 + kappa) * i
-
 
     return [ds_dt, di_dt, dr_dt, dx_dt]
 
 
 class Model:
     """ Base model definition """
+
     CSV_ROW = []
     NUM_PARAMS = 4
     NUM_IC = 4
@@ -107,11 +108,13 @@ class Model:
         num_ic = self.__class__.NUM_IC
 
         if len(p) != num_params or len(initial_conds) != num_ic:
-            raise Exception("Invalid number of parameters \
-                             or initial conditions")
+            raise Exception(
+                "Invalid number of parameters \
+                             or initial conditions"
+            )
         self.p = p
         self.pop = np.sum(initial_conds)
-        self.w0 = initial_conds/self.pop
+        self.w0 = initial_conds / self.pop
         return self
 
     def export(self, f, delimiter=","):
@@ -125,8 +128,7 @@ class Model:
         if self.sol is None:
             raise Exception("Missing call to solve()")
 
-        np.savetxt(f, self.sol, header=",".join(self.__class__.CSV_ROW),
-                   delimiter=",")
+        np.savetxt(f, self.sol, header=",".join(self.__class__.CSV_ROW), delimiter=",")
 
     def fetch(self):
         """ Fetch the data from the model.
@@ -144,9 +146,7 @@ class Model:
         Reference to self
         """
         tspan = np.linspace(0, tf_days, numpoints)
-
-        sol = call_solver(self.__class__.FUNC, self.p, self.w0,
-                          tspan)
+        sol = call_solver(self.__class__.FUNC, self.p, self.w0, tspan)
         # Multiply by the population
         sol[:, 1:] *= self.pop
 
@@ -157,9 +157,9 @@ class Model:
     def r0(self):
         """ Returns reproduction number
         r0 = alpha/beta"""
-        return self.p[0]/self.p[1]
+        return self.p[0] / self.p[1]
 
-    def fit(self, t_obs, n_i_obs, population, inplace=False):
+    def fit(self, t_obs, n_i_obs, population, fit_index=None):
         """ Use the Levenberg-Marquardt algorithm to fit
         the parameter alpha, as beta is assumed constant
 
@@ -171,24 +171,36 @@ class Model:
         Return
         """
 
-        days_obs = t_obs
+        # if no par_index is provided, fit only the first parameter
+        if fit_index is None:
+            fit_index = [False for i in range(len(self.p))]
+            fit_index[0] = True
 
-        def function_handle(t, alpha, beta=self.p[1], population=population):
-            p = [alpha, beta]
-            i_mod = call_solver(self.__class__.FUNC, p, self.w0, t)
+        # Initial values of the parameters to be fitted
+        fit_params0 = np.array(self.p)[fit_index]
+        # Define fixed parameters: this set of parameters won't be fitted
+        # fixed_params = self.p[fix_index]
+
+        def function_handle(t, *par_fit, population=population):
+            params = np.array(self.p)
+            params[fit_index] = par_fit
+            self.p = params
+            i_mod = call_solver(self.__class__.FUNC, self.p, self.w0, t)
             return i_mod[:, 2] * population
 
-        # Fit alpha
-        alpha_opt, pcov = curve_fit(f=function_handle,
-                              xdata=days_obs, ydata=n_i_obs, p0=self.p[0])
-        p_new = np.array(self.p)
-        p_new[0] = alpha_opt[0]
-        self.p = p_new
+        # Fit parameters
+        par_opt, pcov = curve_fit(
+            f=function_handle, xdata=t_obs, ydata=n_i_obs, p0=fit_params0
+        )
+        self.p[fit_index] = par_opt
         self.pcov = pcov
         return self
+        # return p_new, pcov
+
 
 class SIR(Model):
     """ SIR model definition """
+
     CSV_ROW = ["Days", "S", "I", "R"]
     NUM_PARAMS = 2
     NUM_IC = 3
@@ -204,22 +216,24 @@ class SIR(Model):
           n_I0: Toral number of infected
           n_R0: Total number of recovered
           Note n_S0 + n_I0 + n_R0 = Population
-          
+
           Internally, the model initial conditions are the ratios
           S0 = n_S0/Population
           I0 = n_I0/Population
           R0 = n_R0/Population
           which is consistent with the mathematical description
           of the SIR model.
-          
+
         output:
         reference to self
         """
         self._set_params(p, initial_conds)
         return self
 
+
 class SIRX(Model):
     """ SIRX model definition """
+
     CSV_ROW = ["Days", "S", "I", "R", "X"]
     NUM_PARAMS = 4
     NUM_IC = 4
@@ -236,7 +250,7 @@ class SIRX(Model):
           n_R0: Total number of recovered
           n_X0: Total number of quarantined
           Note: n_S0 + n_I0 + n_R0 + n_X0 = Population
-        
+
         Internally, the model initial conditions are the ratios
           S0 = n_S0/Population
           I0 = n_I0/Population
@@ -244,7 +258,7 @@ class SIRX(Model):
           X0 = n_X0/Population
           which is consistent with the mathematical description
           of the SIR model.
-          
+
         output:
         reference to self
         """
