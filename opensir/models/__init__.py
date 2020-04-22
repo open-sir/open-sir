dc33f365647db292dded4768ee8d6e1f92873c30
Jose Alamos <jose.alamos@haw-hamburg.de>
2020-04-22 20:57:27 +0200

    refactor(model): add confidence intervals mixin

--- opensir/models/__init__.py
+++ opensir/models/__init__.py
@@ -2,4 +2,3 @@
 from opensir.models.model import Model
 from opensir.models.sir import SIR
 from opensir.models.sirx import SIRX
-from opensir.models.post_regression import ci_bootstrap, ci_block_cv
